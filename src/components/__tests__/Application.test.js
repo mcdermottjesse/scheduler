ba55d38c493a80848e9240e41d1180c36150a1b4
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-03-23 19:44:35 -0700

    adding tests for on save error

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -15,6 +15,7 @@ import {
 } from "@testing-library/react";
 
 import Application from "components/Application";
+import axios from "__mocks__/axios";
 
 afterEach(cleanup);
 
@@ -87,7 +88,7 @@ describe("Application", () => {
     );
   
     fireEvent.click(getByAltText(appointment, "Delete"));
-    // debug()
+    
     // 4. Check that the confirmation message is shown.
     expect(getByText(appointment, "Are you sure you want to delete")).toBeInTheDocument();
     
@@ -106,9 +107,6 @@ describe("Application", () => {
      queryByText(day, "Monday")
       );
       expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
-
-    // debug()
-
   })
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
@@ -132,7 +130,7 @@ describe("Application", () => {
 
     // 6. Click the "Save" button on that same appointment.
     fireEvent.click(getByText(appointment, "Save"))
-    // debug()
+    
     // 7. Check that the element with the text "Saving" is displayed.
     expect(getByText(appointment, "SAVING")).toBeInTheDocument();
     // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
@@ -143,6 +141,42 @@ describe("Application", () => {
        );
        expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
 
-    debug()
+    
+  })
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce()
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+     // 3. Click the "Add" button on the first empty appointment.
+     const appointments = getAllByTestId(container, "appointment");
+     const appointment = appointments[0];
+     
+     fireEvent.click(getByAltText(appointment, "Add"));
+     // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+       target: { value: "Lydia Miller-Jones" }
+     })
+     // 5. Click the first interviewer in the list.
+     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+     // 6. Click the "Save" button on that same appointment.
+     fireEvent.click(getByText(appointment, "Save"))
+     
+     // 7. Check that the element with the text "Saving" is displayed.
+     expect(getByText(appointment, "SAVING")).toBeInTheDocument();
+    // 8. Wait until the text "Error" is displayed.
+     await waitForElement(() => getByText(container, "Error"));
+     // 9.Check that the error message is shown
+     expect(getByText(appointment,"There was a problem saving your appointment")).toBeInTheDocument();
+     
+     // 10. Check that the DayListItem with the text "Monday" still has the text "1 spot remaining".
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+       );
+       expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+     debug()
   })
 });
